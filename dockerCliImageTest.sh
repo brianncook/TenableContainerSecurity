4cb28178f4ba322747c274f459331c9ffdf8b589
brianncook <30179590+brianncook@users.noreply.github.com>
2018-06-29 10:11:56 -0500

    Update dockerCliImageTest.sh

--- dockerCliImageTest.sh
+++ dockerCliImageTest.sh
@@ -15,7 +15,7 @@
 ACCESS=$(xmlstarlet sel -t -m '//access' -v . -n < /scm/cs-page/data.xml)
 SECRET=$(xmlstarlet sel -t -m '//secret' -v . -n < /scm/cs-page/data.xml)
 
-dockerImages=( "centos:6.7" "centos" "ubuntu:14.04" "ubuntu" "postgres:9.6.9" "alpine:3.2" "alpine:3.4" "postgres:9-alpine" "nginx:1.13" "nginx" "httpd:2.2" "httpd:2.2-alpine" "mongo:3.4" "mongo:3.5" "docker.elastic.co/kibana/kibana:6.2.4" )
+dockerImages=( "centos:6.7" "centos:latest" "ubuntu:14.04" "ubuntu:latest" "postgres:9.6.9" "alpine:3.2" "alpine:3.4" "postgres:9-alpine" "nginx:1.13" "nginx:latest" "httpd:2.2" "httpd:2.2-alpine" "mongo:3.4" "mongo:3.5" "docker.elastic.co/kibana/kibana:6.2.4" )
 
 if [ ! -d "/scm/results" ]; then
   mkdir -p /scm/results
